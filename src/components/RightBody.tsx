2850c988052727cd1c1b3ec0e77ec0fe3a5d39b3
trungtranptn <tantrung.dmc@gmail.com>
2023-03-22 22:45:28 +0700

    todos

--- src/components/RightBody.tsx
+++
@@ -1,9 +0,0 @@
-type props = {};
-
-export const RightBody = (props: props) => {
-  return (
-    <div className="flex text-center border width-30 height-full">
-      <h3>ASIDE</h3>
-    </div>
-  );
-};
